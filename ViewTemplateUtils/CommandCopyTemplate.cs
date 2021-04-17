c66b37ad9410fbfb717a07bf34d1c496b0d85d89
Tereami <undress.my.future@gmail.com>
2021-04-17 20:04:39 +0300

    add license and logs

--- ViewTemplateUtils/CommandCopyTemplate.cs
+++ ViewTemplateUtils/CommandCopyTemplate.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿#region License
+/*Данный код опубликован под лицензией Creative Commons Attribution-ShareAlike.
+Разрешено использовать, распространять, изменять и брать данный код за основу для производных в коммерческих и
+некоммерческих целях, при условии указания авторства и если производные лицензируются на тех же условиях.
+Код поставляется "как есть". Автор не несет ответственности за возможные последствия использования.
+Зуев Александр, 2020, все права защищены.
+This code is listed under the Creative Commons Attribution-ShareAlike license.
+You may use, redistribute, remix, tweak, and build upon this work non-commercially and commercially,
+as long as you credit the author by linking back and license your new creations under the same terms.
+This code is provided 'as is'. Author disclaims any implied warranty.
+Zuev Aleksandr, 2020, all rigths reserved.*/
+#endregion
+#region usings
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.Revit.DB; //для работы с элементами модели Revit
-using Autodesk.Revit.UI; //для работы с элементами интерфейса
-using Autodesk.Revit.UI.Selection; //работы с выделенными элементами
+using System.Diagnostics;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.ApplicationServices;
-
+#endregion
 
 namespace ViewTemplateUtils
 {
@@ -16,6 +28,9 @@ namespace ViewTemplateUtils
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            Debug.Listeners.Clear();
+            Debug.Listeners.Add(new RbsLogger.Logger("CopyTemplates"));
+
             Application app = commandData.Application.Application;
             Document mainDoc = commandData.Application.ActiveUIDocument.Document;
 
@@ -30,8 +45,8 @@ namespace ViewTemplateUtils
                     allDocs.Add(myDoc);
                 }
             }
-
-            if(allDocs.Count ==0)
+            Debug.Write("Docs count: " + allDocs.Count);
+            if(allDocs.Count == 0)
             {
                 message = "Нет открытых документов для копирования!";
                 return Result.Failed;
@@ -39,42 +54,52 @@ namespace ViewTemplateUtils
 
             FormSelectDocument form1 = new FormSelectDocument(allDocs);
             form1.ShowDialog();
-            if (form1.DialogResult != System.Windows.Forms.DialogResult.OK) return Result.Cancelled;
+            if (form1.DialogResult != System.Windows.Forms.DialogResult.OK)
+            {
+                Debug.WriteLine("Cancelled by user");
+                return Result.Cancelled;
+            }
 
             Document selectedDoc = form1.selectedDocument.doc;
+            Debug.WriteLine("Selected doc: " + selectedDoc.Title);
 
             List<View> templates = new FilteredElementCollector(selectedDoc)
                 .OfClass(typeof(View))
                 .Cast<View>()
                 .Where(v => v.IsTemplate == true)
                 .ToList();
+            Debug.WriteLine("Templates found: " + templates.Count);
             List<MyView> myViews = templates
-                .OrderBy(i => i.Name) //ViewName)
+                .OrderBy(i => i.Name) 
                 .Select(i => new MyView(i))
                 .ToList();
 
-
             FormSelectTemplates form2 = new FormSelectTemplates(myViews);
             form2.ShowDialog();
-            if (form2.DialogResult != System.Windows.Forms.DialogResult.OK) return Result.Cancelled;
+            if (form2.DialogResult != System.Windows.Forms.DialogResult.OK)
+            {
+                Debug.WriteLine("Cancelled by user");
+                return Result.Cancelled;
+            }
 
             List<ElementId> templateIds = form2.selectedTemplates.Select(i => i.view.Id).ToList();
+            Debug.WriteLine("Selected templates: " + templateIds.Count);
             CopyPasteOptions cpo = new CopyPasteOptions();
             cpo.SetDuplicateTypeNamesHandler(new DuplicateNamesHandler());
 
             using (Transaction t = new Transaction(mainDoc))
             {
                 t.Start("Копирование шаблонов видов");
-
                 
                 ElementTransformUtils.CopyElements(selectedDoc, templateIds, mainDoc, Transform.Identity, cpo);
-                        
+                
                 t.Commit();
             }
 
             string msg = "Успешно скопировано шаблонов: " + templateIds.Count.ToString();
             if (DuplicateTypes.types.Count > 0) msg += "\nПродублированы: " + DuplicateTypes.ReturnAsString();
 
+            Debug.WriteLine(msg);
             TaskDialog.Show("Отчет", msg);
 
             return Result.Succeeded;
